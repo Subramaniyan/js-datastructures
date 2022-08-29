7ec0a9135627f3bb13f3e8143333297d7323dd2d
Subramaniyan <subramaniyan249@gmail.com>
2022-08-29 18:28:22 +0530

    1 file sum of even indices comments

--- Arrays/sumOfEvenIndices.js
+++ Arrays/sumOfEvenIndices.js
@@ -2,6 +2,10 @@ module.exports = {
   //param A : array of integers
   //param B : array of array of integers
   //return a array of integers
+  //You are given an array A of length N and Q queries given by the 2D array B of size Q*2. Each query consists of two integers B[i][0] and B[i][1].
+  // For every query, the task is to calculate the sum of all even indices in the range A[B[i][0]…B[i][1]].
+
+  // Note : Use 0-based indexing
   solve: function (A, B) {
     let prefixSum = [];
     prefixSum[0] = A[0];
